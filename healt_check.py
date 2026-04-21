b3f703ba49a4f46f8b923e9ccb70da943f265aa5
asdsa <quassimodo.me@gmail.com>
2026-04-21 20:25:57 +0200

    CI pipeline + healt_checking

---
+++ healt_check.py
@@ -0,0 +1,35 @@
+
+import asyncio
+import os
+import sys
+import asyncpg
+from redis.asyncio import Redis
+
+async def check():
+    try:
+
+        conn = await asyncpg.connect(
+        user=os.getenv("DB_USER", "postgres"),
+         password=os.getenv("DB_PASS", "postgres"),
+        database=os.getenv("DB_NAME", "postgres"),
+        host=os.getenv("DB_HOST", "db"), # Обращаемся по внутреннему имени Докера
+        port=5432,
+        timeout=5
+        )
+        await conn.execute("SELECT 1")
+        await conn.close()
+        print("✅PostgreSQL")
+
+        redis = Redis(host="redis", port=6379, socket_timeout=5)
+        await redis.ping()
+        await redis.close()
+        print("✅Redis")
+
+        sys.exit(0)
+
+    except:
+        print(f"❌ ОШИБКА")
+        sys.exit(1)
+
+if __name__ == "__main__":
+  asyncio.run(check())
